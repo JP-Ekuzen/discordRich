d4fc6e19d6fcb5613c696be18acdcc89231d2d29
jp-ekuzen <pwdjpokusa@gmail.com>
2022-06-15 19:26:11 +0200

    Init files

---
+++ start.py
@@ -0,0 +1,83 @@
+from pypresence import Presence
+import time
+from datetime import datetime
+import json
+
+with open("config.json", "r", encoding='utf-8') as config_json_file:
+    config_json = json.load(config_json_file)
+
+active_profile = 'pokemon'
+config_profile = config_json['profiles'][active_profile]
+
+application_id = config_profile['application_id']
+
+profile = config_json['profiles'][active_profile]['presence']
+
+
+def check_if_none(element_name):
+    return profile[element_name] if profile[element_name].strip() != '' else None
+
+def check_if_on(element_name):
+    if profile[element_name].lower().strip() == 'start':
+        return time.time()
+    
+    elif profile[element_name].strip().isnumeric():
+        return profile[element_name].strip()
+
+    else:
+        return None
+
+def check_if_party_is_empty(element_name):
+    if profile[element_name] == [0,0]:
+        return None
+    
+    elif type(profile[element_name]) == list and len(profile[element_name]) == 2 and profile[element_name][0] > 0: 
+        return profile[element_name]
+
+    else:
+        return None
+
+def check_buttons(element_name):
+    buttons_loaded = []
+    if profile[element_name]['first']['label'].strip() != '' and len(profile[element_name]['first']['label'].strip()) > 3:
+        if profile[element_name]['first']['url'].strip() != '' and len(profile[element_name]['first']['url'].strip()) > 3:
+            buttons_loaded.append({"label": profile[element_name]['first']['label'], "url": profile[element_name]['first']['url'].strip()})
+   
+    if profile[element_name]['second']['label'].strip() != '' and len(profile[element_name]['second']['label'].strip()) > 3:
+        if profile[element_name]['second']['url'].strip() != '' and len(profile[element_name]['second']['url'].strip()) > 3:
+            buttons_loaded.append({"label": profile[element_name]['second']['label'], "url": profile[element_name]['second']['url'].strip()})
+
+    if len(buttons_loaded) == 0:
+        return None
+
+    elif 3 > len(buttons_loaded) > 0:
+        return buttons_loaded
+
+def curretly_time():
+    return datetime.now().strftime("%H:%M:%S")
+
+client_id = application_id
+RPC = Presence(client_id)
+RPC.connect()
+
+print("\n" * 100)
+print(f"[{curretly_time()}] Starting...")
+time.sleep(1)
+
+print("\n" * 100)
+print(f"[{curretly_time()}] Started")
+
+RPC.update(
+    details=check_if_none('upper_line_description'),
+    state=check_if_none('lower_line_description'),
+    party_size=check_if_party_is_empty('party_size'),
+    start=check_if_on('start_time_count'),
+    end=check_if_on('end_time_count'),
+    large_image=check_if_none('large_image'),
+    large_text=check_if_none('large_image_description'),
+    small_image=check_if_none('small_image'),
+    small_text=check_if_none('small_image_description'),
+    buttons=check_buttons('button')
+)
+
+input()
